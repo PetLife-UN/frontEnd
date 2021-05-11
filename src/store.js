e2994ac84a76e1d426a3a3e7e8533a70e1bb60ca
Juan Camilo Villota <jcvillotab@unal.edu.co>
2021-05-10 23:17:06 -0500

    First version of new login,register and logout (not functional rn)

---
+++ src/store.js
@@ -0,0 +1,95 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
+import { resolve } from "core-js/fn/promise";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+	state: {
+		statusMessage: "",
+		token: localStorage.getItem("token") || "",
+		user: {},
+		error,
+	},
+	mutations: {
+		login_request(state) {
+			state.statusMessage = "performing login";
+		},
+		login_success(state, token, email) {
+			state.token = token;
+			state.user = email;
+			status.statusMessage = "success";
+		},
+		not_registered_user(state) {
+			status.statusMessage =
+				"El correo ingresado no se encuentra registrado";
+			status.error = true;
+		},
+		credentials_error(state) {
+			status.statusMessage = "La Contraseña ingresada es errónea";
+			status.error = true;
+		},
+		non_activated(state) {
+			status.statusMessage =
+				"El usuario no está activado, actívalo desde tu correo electrónico";
+			status.error = true;
+		},
+		default_error(state, error) {
+			this.statusMessage = error;
+		},
+		logout(state) {
+			state.status = "";
+			state.token = "";
+		},
+	},
+	actions: {
+		login({ commit }, jsonData) {
+			return new Promise((resolve, reject) => {
+				commit("login_request");
+				axios({
+					url: "https://unpetlife.herokuapp.com/api/auth/login",
+					data: jsonData,
+					method: "POST",
+				});
+			})
+				.then((response) => {
+					const token = response.data.data.token;
+					const email = response.data.data.email;
+					localStorage.setItem("token", token);
+					axios.defaults.headers.common["Authorization"] =
+						"Bearer  ${token}";
+					commit("login_success", token, email);
+					resolve(response);
+				})
+				.catch((error) => {
+					const errorMsg = error.response.data.message;
+					switch (errorMsg) {
+						case "Usuario no registrado":
+							commit("not_registered_user");
+							break;
+						case "Error: Unauthorized":
+							commit("credentials_error");
+							break;
+						case "Usuario no activado":
+							commit("non_activated");
+							break;
+						default:
+							commit("default_error", errorMsg);
+							break;
+					}
+					localStorage.removeItem("token");
+					reject(error);
+				});
+		},
+		logout({ commit }) {
+			return new Promise((resolve, reject) => {
+				commit("logout");
+				localStorage.removeItem("token");
+				delete axios.defaults.headers.common["Authorization"];
+				resolve();
+			});
+		},
+	},
+	getters: {},
+});
