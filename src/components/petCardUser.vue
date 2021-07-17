05112b2cae283125c3c83a4f86bb68cbf0fcd271
Juan Camilo Villota Ballesteros <38733879+jcvillotab@users.noreply.github.com>
2021-07-17 11:23:42 -0500

    Fixing petcarduser.vue

--- src/components/petCardUser.vue
+++ src/components/petCardUser.vue
@@ -1,139 +1,187 @@
 <template>
 	<div class="col-lg-4 col-md-6 col-sm-12 vista">
-		
-        <div class="card mb-4 box-shadow" 
-			:class="mascotas()">
+		<div class="card mb-4 box-shadow" :class="mascotas()">
+			<img
+				class="card-img-top imagen_catalogo"
+				v-bind:src="imageRoute"
+				v-bind:alt="idPet"
+			/>
 
-
-            <img class="card-img-top imagen_catalogo" v-bind:src="imageRoute" v-bind:alt="idPet" />
-            
-            <div class="borrar">
-                <button class="boton_borrar" v-on:click="Borrar(idPet)">
-                    <svg 
-                        xmlns="http://www.w3.org/2000/svg" 
-                        class="icon-borrar" 
-                        width="28" 
-                        height="28" 
-                        viewBox="0 0 24 24" 
-                        stroke-width="2" 
-                        stroke="currentColor" 
-                        fill="none" 
-                        stroke-linecap="round" 
-                        stroke-linejoin="round">
-                       
-                       <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                       <line x1="18" y1="6" x2="6" y2="18"></line>
-                       <line x1="6" y1="6" x2="18" y2="18"></line>
-                    </svg>
-                </button>
-            </div>
+			<div class="borrar">
+				<button class="boton_borrar" v-on:click="Borrar(idPet)">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="icon-borrar"
+						width="28"
+						height="28"
+						viewBox="0 0 24 24"
+						stroke-width="2"
+						stroke="currentColor"
+						fill="none"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					>
+						<path
+							stroke="none"
+							d="M0 0h24v24H0z"
+							fill="none"
+						></path>
+						<line x1="18" y1="6" x2="6" y2="18"></line>
+						<line x1="6" y1="6" x2="18" y2="18"></line>
+					</svg>
+				</button>
+			</div>
 
 			<div class="card-body carta_mascota datos_mascota">
-
-
-
-				<div class = "col-9 titulo_masc mayuscula">
-					<h2 :class="nombre(petName)">{{tamanio(petName)}}
-                        <span :class="completo(petName.length)">
-                            {{adicion(petName)}}
-                        </span>
-                    </h2>
-					<p>{{tipo}}  -  {{edad}} años</p>
-                    <p> {{raza}} </p>
+				<div class="col-9 titulo_masc mayuscula">
+					<h2 :class="nombre(petName)">
+						{{ tamanio(petName) }}
+						<span :class="completo(petName.length)">
+							{{ adicion(petName) }}
+						</span>
+					</h2>
+					<p>{{ tipo }} - {{ edad }} años</p>
+					<p>{{ raza }}</p>
 				</div>
 
-				<div class = "col-3 icono">
-                   <!-- <img class="imgmascota" :src="imagen(tipo)">
+				<div class="col-3 icono">
+					<!-- <img class="imgmascota" :src="imagen(tipo)">
  -->
-                    <img v-if="tipo=='Felino'" class="animal imgmascota" src="../assets/img/cat_96px.png">
-                    <img v-else-if="tipo=='Canino'" class="animal imgmascota" src="../assets/img/dog_96px.png">
-                    <img v-else-if="tipo=='Roedor'" class="animal imgmascota" src="../assets/img/hamster_96px.png">
-                    <img v-else-if="tipo=='Pez'" class="animal imgmascota" src="../assets/img/fish_96px.png">
-                    <img v-else-if="tipo=='Bovino'" class="animal imgmascota" src="../assets/img/cow_96px.png">
-                    <img v-else-if="tipo=='Ave'" class="animal imgmascota" src="../assets/img/bird_96px.png">
-                    <img v-else-if="tipo=='Equino'" class="animal imgmascota" src="../assets/img/horse_96px.png">
-                    <img v-else class="animal imgmascota" src="../assets/img/other_96px.png">
-                </div>
-            </div>
-				<!-- <h3 class="card-title">{{ petName }}</h3> -->
+					<img
+						v-if="tipo == 'Felino'"
+						class="animal imgmascota"
+						src="../assets/img/cat_96px.png"
+					/>
+					<img
+						v-else-if="tipo == 'Canino'"
+						class="animal imgmascota"
+						src="../assets/img/dog_96px.png"
+					/>
+					<img
+						v-else-if="tipo == 'Roedor'"
+						class="animal imgmascota"
+						src="../assets/img/hamster_96px.png"
+					/>
+					<img
+						v-else-if="tipo == 'Pez'"
+						class="animal imgmascota"
+						src="../assets/img/fish_96px.png"
+					/>
+					<img
+						v-else-if="tipo == 'Bovino'"
+						class="animal imgmascota"
+						src="../assets/img/cow_96px.png"
+					/>
+					<img
+						v-else-if="tipo == 'Ave'"
+						class="animal imgmascota"
+						src="../assets/img/bird_96px.png"
+					/>
+					<img
+						v-else-if="tipo == 'Equino'"
+						class="animal imgmascota"
+						src="../assets/img/horse_96px.png"
+					/>
+					<img
+						v-else
+						class="animal imgmascota"
+						src="../assets/img/other_96px.png"
+					/>
+				</div>
+			</div>
+			<!-- <h3 class="card-title">{{ petName }}</h3> -->
 			<div class="cboton">
-                <button class="btn btn-lg button_adopta" v-on:click="verInfo(idPet)">Ver mas</button>
-            </div>
+				<button
+					class="btn btn-lg button_adopta"
+					v-on:click="verInfo(idPet)"
+				>
+					Ver mas
+				</button>
+			</div>
 		</div>
 	</div>
 </template>
 
 <script>
+import axios from "axios";
 var datos = '';
 var valor = 1;
 export default {
 	name: "petCardUser",
 	props: ['imageRoute', 'petName', 'petDescription', 'idPet', 'edad', 'tipo', 'raza'],
 	methods: {
-		verInfo(id){
-            this.$router.push("/Info_mascota/"+id)
-        },
+		verInfo(id) {
+			this.$router.push("/Info_mascota/" + id)
+		},
 		goPet() {
 			this.$router.push("/Info_mascota/" + this.idPet)
 		},
-        mascotas() {
-            if(valor%2==0){
-                valor++;
-                return {
-                    'izquierda':true
-                }
-            }
-            else{
-                valor++;
-                return {
-                    'derecha':true
-                } 
-            }
-        },
-        nombre(name) {
-            if(name.length>9){
-                return "max-9 name"
-            }
-            else{
-                return "min-9 name"
-            }
-        },
-        tamanio(nombre) {
-            if(nombre.length < 18) {
-                return nombre
-            }
-            else{
-                datos = nombre.split(" ");
-                // console.log(datos);
-                return `${datos[0]} ${datos[1]}`
-            }
-        },
-        completo(e) {
-            if(e >= 18){
-                return 'mirar'
-            }
-            
-        },
-        adicion(nombre) {
-            if(nombre.length>18){
-                let aux = '';
-                for (var i = 2; i < datos.length; i++) {
-                    aux = aux + ' ' + datos[i];
-                }
-                return aux
-                
-            }
-        },
-        Borrar(idPet) {
-            console.log(idPet);
-        }
+		mascotas() {
+			if (valor % 2 == 0) {
+				valor++;
+				return {
+					'izquierda': true
+				}
+			}
+			else {
+				valor++;
+				return {
+					'derecha': true
+				}
+			}
+		},
+		nombre(name) {
+			if (name.length > 9) {
+				return "max-9 name"
+			}
+			else {
+				return "min-9 name"
+			}
+		},
+		tamanio(nombre) {
+			if (nombre.length < 18) {
+				return nombre
+			}
+			else {
+				datos = nombre.split(" ");
+				// console.log(datos);
+				return `${datos[0]} ${datos[1]}`
+			}
+		},
+		completo(e) {
+			if (e >= 18) {
+				return 'mirar'
+			}
+
+		},
+		adicion(nombre) {
+			if (nombre.length > 18) {
+				let aux = '';
+				for (var i = 2; i < datos.length; i++) {
+					aux = aux + ' ' + datos[i];
+				}
+				return aux
+
+			}
+		},
+		Borrar(idPet) {
+            const token = localStorage.token;
+			axios.
+				put("https://unpetlife.herokuapp.com/api/pet/deletePet"+"?id=" +idPet ,{
+					headers: {
+						Authorization: `Bearer ${token}`,
+					}
+				})
+                .catch((error) => {
+					console.log(error);
+				});
+		}
 	}
 }
 
 </script>
 
 <style scoped>
-
 .btn-lg {
 	margin-left: 5px;
 	margin-right: 5px;
--- src/views/profile/Profile.vue
+++ src/views/profile/Profile.vue
@@ -116,185 +116,182 @@ var A_apellido = '';
 var A_numero = 0;
 
 export default {
-  name: "Profile",
-  data() {
-    return {
-      show: true,
-      json: null,
-      errorBool: false,
-      e_nombre: false,
-      e_apellido: false,
-      e_phone: false
-    };
-  },
-  components: {
-    navbar,
-  },
-  methods:{
-    errorNombre() {
-        this.limpiar('#floatingName');
-        this.e_nombre=false;
-    },
-    errorApellido() {
-        this.limpiar('#floatingSurame');
-        this.e_apellido=false;
-    },
-    errorCelular() {
-        this.limpiar('#floatingcellPhoneNumber');
-        this.e_phone = false;
-    },
-    limpiar(a) {
-        const error_registro = document.querySelector(a);
-        error_registro.classList.remove('error');
-    },
-    goRegister(){
-        this.$router.push('animalreg')
-    },
-    goApliAdopcion(){
-        this.$router.push('/profile/consultaapli/1')
-    },
-    goUserPets(){
-        this.$router.push('/user/userPet')
-    },
-    cancelar(){
-      this.json.name = A_nombre;
-      this.json.surname = A_apellido;
-      this.json.cellPhoneNumber = A_numero;
-    },
-    editado(){
-      if(this.show){
-        var constante = true;
-        const nombre = document.querySelector('#floatingName');
-        // console.log(nombre.value.length);
-        const apellido = document.querySelector('#floatingSurame');
-        const celular = document.querySelector('#floatingcellPhoneNumber');
+	name: "Profile",
+	data() {
+		return {
+			show: true,
+			json: null,
+			errorBool: false,
+			e_nombre: false,
+			e_apellido: false,
+			e_phone: false
+		};
+	},
+	components: {
+		navbar,
+	},
+	methods: {
+		errorNombre() {
+			this.limpiar('#floatingName');
+			this.e_nombre = false;
+		},
+		errorApellido() {
+			this.limpiar('#floatingSurame');
+			this.e_apellido = false;
+		},
+		errorCelular() {
+			this.limpiar('#floatingcellPhoneNumber');
+			this.e_phone = false;
+		},
+		limpiar(a) {
+			const error_registro = document.querySelector(a);
+			error_registro.classList.remove('error');
+		},
+		goRegister() {
+			this.$router.push('animalreg')
+		},
+		goApliAdopcion() {
+			this.$router.push('/profile/consultaapli/1')
+		},
+		goUserPets() {
+			this.$router.push('/user/userPet')
+		},
+		cancelar() {
+			this.json.name = A_nombre;
+			this.json.surname = A_apellido;
+			this.json.cellPhoneNumber = A_numero;
+		},
+		editado() {
+			if (this.show) {
+				var constante = true;
+				const nombre = document.querySelector('#floatingName');
+				// console.log(nombre.value.length);
+				const apellido = document.querySelector('#floatingSurame');
+				const celular = document.querySelector('#floatingcellPhoneNumber');
 
-        var m = nombre.value.split(" ");
-        var B_nombre = "";
+				var m = nombre.value.split(" ");
+				var B_nombre = "";
 
-        for (var i = 0; i < m.length; i++) {
-          if(m[i] != ""){
-            if(B_nombre != "") B_nombre = B_nombre + " ";
-            B_nombre = B_nombre + m[i];
-          }
-        }
-        m = apellido.value.split(" ");
-        var B_apellido = "";
+				for (var i = 0; i < m.length; i++) {
+					if (m[i] != "") {
+						if (B_nombre != "") B_nombre = B_nombre + " ";
+						B_nombre = B_nombre + m[i];
+					}
+				}
+				m = apellido.value.split(" ");
+				var B_apellido = "";
 
-        for (i = 0; i < m.length; i++) {
-          if(m[i] != ""){
-            if(B_apellido != "") B_apellido = B_apellido + " ";
-            B_apellido = B_apellido + m[i];
-          }
-        }
-        // console.log(B_nombre.length);
-        while (document.querySelector('.error') != null) {
-          document.querySelector('.error').classList.remove('error');
-        }
-        if (B_nombre.length === 0 || /\d/.test(B_nombre)) {
-          nombre.classList.add('error');
-          this.e_nombre = true
-        }
-        if (apellido.value.length === 0 || /\d/.test(apellido.value)) {
-          apellido.classList.add('error');
-          this.e_apellido = true
-        }
-        if (celular.value.length < 8) {
-          celular.classList.add('error');
-          this.e_phone = true;
-        }
-        console.log(`${this.json.name} != ${B_nombre} || ${this.json.surname} != ${B_apellido} || ${this.json.cellPhoneNumber} != ${celular.value}`);
+				for (i = 0; i < m.length; i++) {
+					if (m[i] != "") {
+						if (B_apellido != "") B_apellido = B_apellido + " ";
+						B_apellido = B_apellido + m[i];
+					}
+				}
+				// console.log(B_nombre.length);
+				while (document.querySelector('.error') != null) {
+					document.querySelector('.error').classList.remove('error');
+				}
+				if (B_nombre.length === 0 || /\d/.test(B_nombre)) {
+					nombre.classList.add('error');
+					this.e_nombre = true
+				}
+				if (apellido.value.length === 0 || /\d/.test(apellido.value)) {
+					apellido.classList.add('error');
+					this.e_apellido = true
+				}
+				if (celular.value.length < 8) {
+					celular.classList.add('error');
+					this.e_phone = true;
+				}
+				console.log(`${this.json.name} != ${B_nombre} || ${this.json.surname} != ${B_apellido} || ${this.json.cellPhoneNumber} != ${celular.value}`);
 
-        // console.log("no autualizacion");
-        if(this.e_nombre || this.e_apellido || this.e_phone) {
-          console.log("no autualizacion");
+				// console.log("no autualizacion");
+				if (this.e_nombre || this.e_apellido || this.e_phone) {
+					console.log("no autualizacion");
 
-          this.show = !this.show;
-        }
-        else if (A_nombre != B_nombre || A_apellido != B_apellido || A_numero != celular.value) {
-          
-          console.log("autualizacion");
-          
-          let json = {
-            "name" : B_nombre,
-            "surname" : B_apellido,
-            "cellPhoneNumber" : celular.value
-          }
-          axios.put("https://unpetlife.herokuapp.com/api/user/modifyUserDetails",json, {
-            
-            headers: {
-              Authorization: `Bearer ${token}`,
-            }
-          })
-          // {
-          // "name": "Juan Camilo",
-          // "surname": "Villota",
-          // "cellPhoneNumber": "3144463105"
-          // } 
-        }
-        else {
-          this.json.name = A_nombre;
-          this.json.surname = A_apellido;
-          this.json.cellPhoneNumber = A_numero;
-      },
-      UltimaCon(js){
-        if(js != null){
-            var date = new Date(js.lastLogin);
-            date.setMinutes(date.getMinutes() - date.getTimezoneOffset());
-            return `${date.toLocaleString()}`;
-        }
-      }
-    },
-    Nombre(js) {
-      if(js != null){
-        A_nombre = js.name;
-        A_apellido = js.surname;
-        return `${js.name} ${js.surname}`;
-      }
-    },
-    Email(js) {
-      if(js != null){
-        return `${js.email}`;
-      }
-    },
-    Cell(js) {
-      if(js != null){
-        A_numero = js.cellPhoneNumber;
-        return `${js.cellPhoneNumber}`;
-      }
-    },
-    Rol(js) {
-      if(js != null){
-        
-        return `${js.roles}`;
-      }
-    },
-    UltimaCon(js){
-      if(js != null){
-        return localStorage.horaultima;
-      }
-    }
-  },
-  mounted:function(){
-    axios
-      .get("https://unpetlife.herokuapp.com/api/user/getUserDetails", {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          }
-      })
-      .then((data) => {
-        if(data.data != null) {
-          this.json = data.data;
-        }
-        // console.log(this.json);
-      })
-      .catch((error) => {
-        if (error.response.status === 404) {
-          this.errorBool = true;
-        }
-      });
-    
-  }
+					this.show = !this.show;
+				}
+				else if (A_nombre != B_nombre || A_apellido != B_apellido || A_numero != celular.value) {
+
+					console.log("autualizacion");
+
+					let json = {
+						"name": B_nombre,
+						"surname": B_apellido,
+						"cellPhoneNumber": celular.value
+					}
+					axios.put("https://unpetlife.herokuapp.com/api/user/modifyUserDetails", json, {
+
+						headers: {
+							Authorization: `Bearer ${token}`,
+						}
+					})
+					// {
+					// "name": "Juan Camilo",
+					// "surname": "Villota",
+					// "cellPhoneNumber": "3144463105"
+					// } 
+				}
+				else {
+					this.json.name = A_nombre;
+					this.json.surname = A_apellido;
+					this.json.cellPhoneNumber = A_numero;
+				}
+			}
+		},
+		Nombre(js) {
+			if (js != null) {
+				A_nombre = js.name;
+				A_apellido = js.surname;
+				return `${js.name} ${js.surname}`;
+			}
+		},
+		Email(js) {
+			if (js != null) {
+				return `${js.email}`;
+			}
+		},
+		Cell(js) {
+			if (js != null) {
+				A_numero = js.cellPhoneNumber;
+				return `${js.cellPhoneNumber}`;
+			}
+		},
+		Rol(js) {
+			if (js != null) {
+
+				return `${js.roles}`;
+			}
+		},
+		UltimaCon(js) {
+			if (js != null) {
+				var date = new Date(js.lastLogin);
+				date.setMinutes(date.getMinutes() - date.getTimezoneOffset());
+				return `${date.toLocaleString()}`;
+			}
+
+		}
+	},
+	mounted: function () {  
+		axios
+			.get("https://unpetlife.herokuapp.com/api/user/getUserDetails", {
+				headers: {
+					Authorization: `Bearer ${token}`,
+				}
+			})
+			.then((data) => {
+				if (data.data != null) {
+					this.json = data.data;
+				}
+				// console.log(this.json);
+			})
+			.catch((error) => {
+				if (error.response.status === 404) {
+					this.errorBool = true;
+				}
+			});
+
+	}
 };
 </script>
 <style >
